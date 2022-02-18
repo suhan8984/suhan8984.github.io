977728d2ecd10d30a39afe0fc7ec117e2ad49d1a
suhan8984 <55016623+suhan8984@users.noreply.github.com>
2022-02-18 20:38:03 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var clock;
+
+window.addEventListener('load', displayClock, false);
+window.addEventListener('unload', stopClock, false);
+
+
+function preset(tt){
+    if(tt < 10){
+        return "0" + tt;
+    }
+    else{
+        return tt;
+    }
+}
+function stopClock(){
+    setTimeout(clock);
+}
+
+function displayClock(){
+    var now = new Date();
+    var hourNow = preset(now.getHours());
+    var minuteNow = preset(now.getMinutes());
+    var secondNow = preset(now.getSeconds());
+
+    document.digital_clock.time_val.value = hourNow + ":" + minuteNow + ":" + secondNow;
+    clock = setTimeout(displayClock, 1000);
+}
+
+jQuery(function($){
+    $('#bulletLooper').on('shown', function(e){
+        $('.looper-nav > li', this)
+            .removeClass('active')
+            .eq(e.relatedIndex)
+            .addClass('active');
+    });
+});
+
